d6c06197ce53a3dada8d0e6d1a8eca014f3c6a70
HtppsPratik <pratik.thapa.1223@gmail.com>
2024-01-09 14:03:16 +0545

    pdf

---
+++ repos/Eventloop/Eventloop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Eventloop
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Event loop started");
+
+            for (int i= 0; i < 4 ; i++)
+            {
+                Console.WriteLine($"Happy New year {i + 1 }");
+                System.Threading.Thread.Sleep( 5 );
+
+            }
+            Console.WriteLine("Event loop Finished");
+        }
+    }
+}
